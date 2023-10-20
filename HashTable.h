c1ab671c4dbc9ff706129ef1f46fd4763e51e304
RazmikManukyan <107432984+RazmikManukyan@users.noreply.github.com>
2023-10-21 00:02:39 +0400

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -4,21 +4,22 @@
 #include "Vector/Vector.h"
 #include "forward_list/FList.h"
 #include "MyUtils.h"
-
+#include <optional>
 
 class HashTable {
 private:
     Vector<FList<MyUtils::KeyValue<int, int>>> map;
     int tableSize;
 
-    int hashFunction(int);
+    int hashFunction(int) const;
 public:
-    HashTable(int);
+    explicit HashTable(int);
 
     void insert(int, int);
     int get(int);
     void remove(int);
 
+    int GetSize() const;
 };
 
 #endif //HASHTABLE_H
